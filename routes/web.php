ebdf5072ab01fb98e909975331d7443dca6fa171
DavidPisuna <brayanpisuna@outlook.es>
2024-04-24 21:06:04 -0500

    Editar Usuario Perfil

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\AdminProfileController;
+
 /*
+
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -34,10 +37,13 @@ require __DIR__.'/auth.php';
 
 
 ////Creamos la ruta
+//Creamos una controlador y creamos una ruta nueva
 ///aqui protegemos la ruta si no es administrador no refleja
 
 Route::middleware('auth','role:admin')->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/profile', [AdminProfileController::class, 'index'])->name('admin.profile');
+    Route::post('/admin/profile/update', [AdminProfileController::class, 'updateProfile'])->name('admin.profile.update');
 });
 
 Route::middleware('auth','role:vendor')->group(function () {
@@ -46,3 +52,4 @@ Route::middleware('auth','role:vendor')->group(function () {
 
 ////Creamos una ruta unica para el administrador que sea publica
 Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
+
